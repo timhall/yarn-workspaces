e87dc6a4555ef754d3463b1f13ef4571343ea67f
Tim Hall <tim.hall.engr@gmail.com>
2020-03-05 21:52:33 -0500

    SAVEPOINT

--- packages/yarn-workspaces-foreach/src/index.ts
+++ packages/yarn-workspaces-foreach/src/index.ts
@@ -1,3 +1,28 @@
-export function foreach() {
+import findWorkspaceRoot from 'find-yarn-workspace-root';
+import { cpus } from 'os';
+import { listWorkspaces } from 'yarn-workspaces-list';
+
+interface ForeachOptions {
+  cwd?: string;
+  parallel?: boolean;
+  jobs?: number;
+  topological?: boolean;
+  include?: string[];
+  exclude?: string[];
+}
+
+export async function foreach(options: ForeachOptions = {}): Promise<void> {
+  const {
+    cwd = process.cwd(),
+    parallel = false,
+    jobs = Math.max(1, cpus().length / 2),
+    topological = false,
+    include = [],
+    exclude = []
+  } = options;
+
+  const list = await listWorkspaces({ cwd });
+  const root = findWorkspaceRoot(cwd);
+
   // TODO
 }
