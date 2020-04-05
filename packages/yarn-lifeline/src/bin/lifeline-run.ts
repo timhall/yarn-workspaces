18b5b33a554a9444d2b37ce2680ea1fdf57f198f
Tim Hall <tim.hall.engr@gmail.com>
2020-04-04 22:11:12 -0400

    Pass arguments to run

--- packages/yarn-lifeline/src/bin/lifeline-run.ts
+++ packages/yarn-lifeline/src/bin/lifeline-run.ts
@@ -21,8 +21,7 @@ export default async function(argv: string[]): Promise<void> {
     return;
   }
 
-  const command = args._;
   const config = await loadConfig(cwd);
 
-  await run(command.join(' '), config, { cwd });
+  await run(argv.join(' '), config, { cwd });
 }
